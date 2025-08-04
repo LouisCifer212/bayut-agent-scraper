e4d1af3b6cb56ae66dc779c636b2a512ae6b2630
LouisCifer212 <Luigi@creativeislandmedia.com>
2025-08-04 17:10:30 +0400

    Update bayut_whatsapp_scraper.py

--- bayut_whatsapp_scraper.py
+++ bayut_whatsapp_scraper.py
@@ -10,6 +10,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--single-process")
+chrome_options.add_argument("--disable-software-rasterizer")
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 import re
